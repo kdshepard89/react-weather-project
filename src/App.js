bc8d9a2915c197171a47ed95aba122760078b817
Katie S <kdshepard89@gmail.com>
2023-04-23 09:25:18 -0400

    added a link to GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1 className="App-header">Weather App</h1>
+      <footer>
+        This project is created by Katie Shepard and is open sourced on{" "}
+        <a
+          href="https://github.com/kdshepard89/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
