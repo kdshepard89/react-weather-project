e716d8a16d29713c20627b6b0ee565b0cbdd08ca
Katie S <kdshepard89@gmail.com>
2023-04-24 09:01:17 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="App-header">Weather App</h1>
-      <footer>
-        This project is created by Katie Shepard and is open sourced on{" "}
-        <a
-          href="https://github.com/kdshepard89/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1 className="App-header">Weather App</h1>
+        <footer>
+          This project is created by Katie Shepard and is open sourced on{" "}
+          <a
+            href="https://github.com/kdshepard89/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
