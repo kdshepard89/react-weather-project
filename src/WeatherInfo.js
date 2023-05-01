f1b416ca256353d7fe64a0b1767c860ea226249d
Katie S <kdshepard89@gmail.com>
2023-05-01 10:59:51 -0400

    imported animated icons and interpolated with API

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,13 +14,11 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
-            <div className="float-left">
+          <div className="d-flex">
+            <div>
+              <WeatherIcon code={props.data.icon} />
+            </div>
+            <div>
               <span className="temperature">
                 {Math.round(props.data.temperature)}
               </span>
